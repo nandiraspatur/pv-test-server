aa772ed9ae5973ad3bab684003074d10481fdbb9
Nandira Paturohman <nandirasp@gmail.com>
2018-01-26 21:05:58 +0700

    add cors

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const logger = require('morgan');
+const cors = require('cors')
 const mongoose = require('mongoose')
-
+ 
 mongoose.connect('mongodb://vinnixdb:ShadowFax5@cluster0-shard-00-00-b8rmh.mongodb.net:27017,cluster0-shard-00-01-b8rmh.mongodb.net:27017,cluster0-shard-00-02-b8rmh.mongodb.net:27017/pvtest?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
-// Routes
 const events = require('./routes/events');
 const booking = require('./routes/booking');
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
